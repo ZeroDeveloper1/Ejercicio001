5037ff2c6632b031784e15337aa34cb58375ce8b
franklinwbp <franklinwbp@gmail.com>
2024-08-21 20:17:48 -0500

    Ejercicios

---
+++ 1Tringulo.py
@@ -0,0 +1,9 @@
+# Guardar como triangle.py
+def create_triangulo():
+    for i in range(1, 20):
+        line = ''
+        for i in range(i):
+            line += '#'
+        print(line)
+
+create_triangulo()

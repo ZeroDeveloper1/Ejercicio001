a04c79623b6869e2aa8156c91fb6e7e024a11458
FranklinBP <162067414+franklinbp@users.noreply.github.com>
2025-02-07 17:51:46 -0500

    practica

---
+++ lista.py
@@ -0,0 +1,9 @@
+def filtaryordenar(numeros):
+    filtrados = [num for num innumeros if num % 2 !=0]
+    return sorted(filtrados,reverse=true)
+
+numeros = [1, 5, 5, 2, 6, 10]
+resultados = filtaryordenar(numeros)
+
+print("lista origianal")
+print("listafirltrada y ordenada")
